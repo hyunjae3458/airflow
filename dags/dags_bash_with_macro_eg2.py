9e22c8429bdd9159324cfc8ed15bb507d67c5295
hyunjaekim <fredsonata@gmail.com>
2025-09-15 18:21:03 +0900

    bash 날짜 merco 수정2

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
-    dag_id = "dags_bash_with_macro_eg2",
+    dag_id = "dags_bash_with_mecro_eg2",
     schedule="10 0 * * 6#2", # 매월 둘째주 토요일
     start_date=pendulum.datetime(2025, 9 , 1, tz="Asia/Seoul"),
     catchup=False 
