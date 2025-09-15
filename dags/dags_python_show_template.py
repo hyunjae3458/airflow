631e1d2865325f899613211dfee2baa74b074c00
hyunjaekim <fredsonata@gmail.com>
2025-09-15 17:01:16 +0900

    task decorator tamplate 업로드

---
+++ dags/dags_python_show_template.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id = "dags_python_show_template",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 9 , 1, tz="Asia/Seoul"),
+    catchup=True # 9월 1일부터 9월 15일까지 구간 모두 수행
+) as dag:
+    
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import print # 이쁘게 print해주는 출력문
+        pprint(kwargs)
+
+    show_templates()
