68a549339533d8232043356e19b7d4ccb4136a2e
hyunjaekim <fredsonata@gmail.com>
2025-09-15 15:39:03 +0900

    python op_kwargs 수정

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -2,7 +2,7 @@ from airflow.sdk import DAG
 import pendulum
 import datetime
 from airflow.providers.standard.operators.python import PythonOperator
-from common.common_func import regist
+from common.common_func import regist2
 
 with DAG(
     dag_id = "dags_python_with_op_kwargs",
