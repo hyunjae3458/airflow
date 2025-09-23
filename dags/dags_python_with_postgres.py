f6d82f0dad9dd1872a2c92abe0f8684679899277
hyunjaekim <fredsonata@gmail.com>
2025-09-23 15:25:08 +0900

    postgres에 데이터 insert 업로드

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -20,7 +20,7 @@ with DAG(
                 run_id = kwargs.get("ti").run_id
                 msg = "insrt 수행"
                 sql = "insert into py_opr_drct_insrt values (%s,%s,%s,%s);"
-                cursor.execute(sql,(dag_id,task_id,run_id,masg))
+                cursor.execute(sql,(dag_id,task_id,run_id,msg))
                 conn.commit()
 
     insrt_postgres = PythonOperator(
