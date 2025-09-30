b5785617cc5f101103de47fac3f744684cedbc43
hyunjaekim <fredsonata@gmail.com>
2025-09-30 17:43:06 +0900

    custom sensor 업로드

--- dags/dags_custom_sensor.py
+++ dags/dags_custom_sensor.py
@@ -3,7 +3,7 @@ import pendulum
 from sensors.seoul_api_data_sensor import SeoulApiDataSensor
 
 with DAG(
-    dag_id = "dags_external_task_sensor",
+    dag_id = "dags_custom_sensor",
     schedule="0 7 * * *",
     start_date=pendulum.datetime(2025, 9 , 1, tz="Asia/Seoul"),
     catchup=False 
